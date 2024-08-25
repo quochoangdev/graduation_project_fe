1a9a8c1c772d2c8e1842e38e9c59336bafae15b0
lcanh28 <lcanh2003@gmail.com>
2024-08-25 16:25:07 +0700

    create page profile

---
+++ src/frontend/pages/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import classNames from 'classnames/bind'
+import styles from './Profile.module.scss'
+
+const cx = classNames.bind(styles)
+
+const Profile = () => {
+  return (
+    <div className={cx('')}>Profile</div>
+  )
+}
+
+export default Profile
